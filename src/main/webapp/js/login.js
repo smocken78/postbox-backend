6d6c9e295a5a277c46884b9d2f0142720a1da255
smocken <smocken@NBL21122.congstar.local>
2022-12-23 14:39:34 +0100

    .

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,29 @@
+async function login (user,password) {
+
+
+		document.querySelector("#msg").innerHTML = 
+		`<div class="spinner-border" role="status">
+		  <span class="sr-only">Loading...</span>
+		</div>`;
+		
+		const config = {
+	        method: 'POST',
+	        headers: {
+	            'Accept': 'application/json',
+	            'Content-Type': 'application/json',
+	        },
+	        body: "email="+user+"&password="+password
+    	}
+		
+		const response = await fetch ('/postbox/login',config);
+		
+		if (response.ok) {
+			console.log("Okay");
+		}
+		else {
+		    document.querySelector("#msg").innerHTML = "Das hat nicht funktioniert";
+		}
+
+		  
+};
+
