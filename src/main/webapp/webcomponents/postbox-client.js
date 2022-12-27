c78f3d81aea2f44a72328ae9d33fe7eb89f227d1
smocken <smocken@NBL21122.congstar.local>
2022-12-27 10:26:56 +0100

    Merge branch 'main' of https://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@github.com/smocken78/postbox-backend.git into main

--- src/main/webapp/webcomponents/postbox-client.js
+++ src/main/webapp/webcomponents/postbox-client.js
@@ -36,48 +36,53 @@ class PostboxClient extends HTMLElement {
 		let files; 
 		
 		try {
-			files = await response.json();
-			const options = { day: "2-digit", month: "2-digit", year: "numeric" };
 			
-			if (files.length == 0) {
-				document.querySelector("#result").innerHTML = `Nix da`;
+			if (response.status != 200) {
+				window.location = './'
 			}
 			else {
-			var arr = [];
-			arr.push(html `<div class="row">`);
-			files.forEach((item) => {
+				files = await response.json();
+				const options = { day: "2-digit", month: "2-digit", year: "numeric" };
+			
+				if (files.length == 0) {
+					document.querySelector("#result").innerHTML = `Nix da`;
+				}
+				else {
+				var arr = [];
+				arr.push(html `<div class="row">`);
+				files.forEach((item) => {
+					
+					const date = new Date(item["insertation_dt"]);
+				    const d = date.toLocaleDateString("de-DE", options);
+			    	
+					arr.push(html `
+					<div class="col-12">
+					<a href='#' id='${item["filename"]}'
+					  @click=${(event) => {
+	                 	event.preventDefault();
+	                 	this.openEmail(item["filename"]);
+	               			}} 
+	               			 class="list-group-item list-group-item-action flex-column align-items-start">
+					    <div class="d-flex w-100 justify-content-between">
+					      <h5 class="mb-1">${item["subject"]}</h5>
+					      <small>${d}</small>
+					    </div>
+					    <p class="mb-1">${item["preview"]}</p>
+					    
+					  </a>
+					  </div>
+					  `);
 				
-				const date = new Date(item["insertation_dt"]);
-			    const d = date.toLocaleDateString("de-DE", options);
-		    	
-				arr.push(html `
-				<div class="col-12">
-				<a href='#' id='${item["filename"]}'
-				  @click=${(event) => {
-                 	event.preventDefault();
-                 	this.openEmail(item["filename"]);
-               			}} 
-               			 class="list-group-item list-group-item-action flex-column align-items-start">
-				    <div class="d-flex w-100 justify-content-between">
-				      <h5 class="mb-1">${item["subject"]}</h5>
-				      <small>${d}</small>
-				    </div>
-				    <p class="mb-1">${item["preview"]}</p>
-				    
-				  </a>
-				  </div>
-				  `);
-			
-		    });
-		      
-		     arr.push(html `</div>
-		      <div class="col-8">
-				   <div id="msg"></div>
-				  </div>
-			   </div>`);
-			   render(arr,document.querySelector("#result"));
+			    });
+			      
+			     arr.push(html `</div>
+			      <div class="col-8">
+					   <div id="msg"></div>
+					  </div>
+				   </div>`);
+				   render(arr,document.querySelector("#result"));
+				}
 			}
-			
 		
 		}
 		catch (e){
